61fa4836737b5e137b038cf889be94a22b4b1115
dhakedrimjhim31 <91783657+dhakedrimjhim31@users.noreply.github.com>
2022-05-24 12:25:13 +0530

    Create day 29 .cpp

---
+++ RIMJHIM/30 Days/day 29 .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+void print(int i) {
+  cout << " Here is int " << i << endl;
+}
+void print(double  f) {
+  cout << " Here is float " << f << endl;
+}
+void print(char const *c) {
+  cout << " Here is char* " << c << endl;
+}
+ 
+int main() {
+  print(60);
+  print(40.10);
+  print("three");
+  return 0;
+}
